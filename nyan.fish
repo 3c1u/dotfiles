025a4daf5b76fb839c0367674411ba59e4933cbc
Hikaru Terazono <3c1u@vulpesgames.tokyo>
2020-07-09 00:54:48 +0900

    Create nyan.fish

---
+++ nyan.fish
@@ -0,0 +1,8 @@
+source ~/.cargo/env
+
+set -x PATH $HOME/Qt/5.15.0/clang_64/bin $HOME/.local/share/flutter/bin $HOME/.local/bin $PATH
+set -x PATH /Applications/UpTeX.app/Contents/Resources/TEX/texbin $PATH
+
+alias cls="clear"
+alias vi="emacs -nw"
+alias vim="emacs -nw"
